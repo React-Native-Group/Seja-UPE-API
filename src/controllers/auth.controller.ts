43f65304336ffb88f45eeb9c1dac0fb007ec393d
MurylloEx <muryllopimenta@gmail.com>
2021-11-25 14:59:42 -0300

    :sparkles: feature: Added OAuth2 service for validating tokens.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 
-@ApiTags('authentication')
-@Controller('auth')
+@ApiTags("authentication")
+@Controller("auth")
 export class AuthController {
 
-  
+  @Get("")
+  onAuthRequested(){
+
+  }
 
 }
---
+++ src/services/oauth2.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { OAuth2Client } from "google-auth-library";
+import { OAuth2 } from 'src/config/server.json';
+
+const client = new OAuth2Client(OAuth2.realClientId);
+
+@Injectable()
+export class OAuth2Service {
+
+  async verifyIdToken(token: string) {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: [OAuth2.expoClientId, OAuth2.realClientId]
+    });
+    return ticket.getPayload();
+  }
+
+}
