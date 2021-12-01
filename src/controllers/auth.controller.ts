f42f74de5946953ed2de7bd6a8dcb80693ed14f9
MurylloEx <muryllopimenta@gmail.com>
2021-12-01 15:37:18 -0300

    :poop: fix: Added again the commented auth code.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -22,20 +22,20 @@ export class AuthController {
   @Post("authorize")
   async onAuthRequested(@Body() body: AuthorizationValidator)
   {
-    // let payload = await this.oauth2Service.verifyIdToken(body.idToken);
-    // if (!payload)
-    //   throw new OAuth2UnauthorizedException();
+    let payload = await this.oauth2Service.verifyIdToken(body.idToken);
+    if (!payload)
+      throw new OAuth2UnauthorizedException();
 
-    // const { email, name } = payload;
+    const { email, name } = payload;
 
-    // if (await this.authService.getPreviousAuthorization(body.idToken, payload)){
-    //   await this.mailerService.sendWelcomeMail(name, email);
-    // }
-    return await this.authService.buildToken({})
-    // return {
-    //   bearer: await this.authService.buildToken(payload),
-    //   payload
-    // };
+    if (await this.authService.getPreviousAuthorization(body.idToken, payload)){
+      await this.mailerService.sendWelcomeMail(name, email);
+    }
+    
+    return {
+      bearer: await this.authService.buildToken(payload),
+      payload
+    };
   }
 
 }
