c2076b0f3108504a427a1a607034dbb60f7d7a0d
MurylloEx <muryllopimenta@gmail.com>
2021-12-02 02:48:19 -0300

    :alembic: feature: Increase Timeout value of TimeoutInterceptor.

--- src/hooks/timeout.interceptor.ts
+++ src/hooks/timeout.interceptor.ts
@@ -5,7 +5,7 @@ import { catchError, Observable, throwError, timeout, TimeoutError } from 'rxjs'
 export class TimeoutInterceptor implements NestInterceptor {
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(timeout(10000), catchError(error => {
+    return next.handle().pipe(timeout(25000), catchError(error => {
       if (error instanceof TimeoutError)
         return throwError(() => new RequestTimeoutException('A solicitação demorou muito tempo para responder!'));
       return throwError(() => error);
