784fbd2f35f0a3515bc2a90df7a3f230176c4c9b
MurylloEx <muryllopimenta@gmail.com>
2021-12-02 15:24:10 -0300

    :sparkles: feature: Added new fetch functions to EvaluationService.

--- src/services/evaluation.service.ts
+++ src/services/evaluation.service.ts
@@ -6,6 +6,16 @@ import { PopularityValidator } from "src/validators";
 import { CourseNotFoundException } from "src/exceptions";
 import { CourseModel, PopularityModel, RatingModel } from "src/models";
 
+export type PopularityInfo = {
+  likes: number;
+  dislikes: number;
+}
+
+export type RatingInfo = {
+  average: number;
+  total: number;
+}
+
 @Injectable()
 export class EvaluationService {
 
@@ -22,20 +32,43 @@ export class EvaluationService {
   
   ){}
 
-  async submitRating(note: number){
+  async submitRating(note: number)
+  {
     let rate = await this.rating.create({ note });
     return await this.rating.save(rate);
   }
   
-  async submitCoursePopularity({ courseId, value }: PopularityValidator){
+  async submitCoursePopularity({ courseId, value }: PopularityValidator)
+  {
     let course = await this.courses.findOne({ id: courseId });
-
     if (!course)
       throw new CourseNotFoundException();
     
     let rate = await this.popularity.create({ course, value });
-    
     return await this.popularity.save(rate);
+  }
+
+  async fetchRatings()
+  {
+    let ratings = await this.rating.find();
+    let sum = ratings.reduce((a,rate) => a + rate.note, 0);
+
+    let total = ratings.length;
+    let average = sum / total;
+
+    return { average, total }
+  }
+
+  async fetchPopularity(courseId: number): Promise<PopularityInfo>
+  {
+    let course = await this.courses.findOne({ id: courseId });
+    if (!course)
+      throw new CourseNotFoundException();
+
+    let likes = await this.popularity.count({ course, value: 'like' });
+    let dislikes = await this.popularity.count({ course, value: 'dislike' });
+
+    return { likes, dislikes };
   }
 
 }
